63ffa102723e17748c404330cab7ecc8aa920f0c
jruizsilva <ruizsilvajonathan@gmail.com>
2023-09-18 10:51:57 -0300

    dockerfile added

--- database/src/index.js
+++ database/src/index.js
@@ -1,6 +1,6 @@
 import server from './server'
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
   console.log(`Database service is running on port ${port}`)
